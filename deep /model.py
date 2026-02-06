5af17e3a6acd70e85c40f9f8000bde31cd9f3e69
Rácz Nikoletta <44648333+RaczNikoletta@users.noreply.github.com>
2026-02-06 07:33:26 +0100

    Create initial Keras model structure in model.py
    Define a Keras sequential model with two hidden layers and an output layer.

---
+++ deep /model.py
@@ -0,0 +1,19 @@
+from tensorflow import keras
+
+#Number of classes in the target variable
+NB_CLASSES = 3
+
+#Create a sequencial model in Keras
+model = tf.keras.models.Sequential()
+
+#Add the first hidden layer
+model.add(keras.layers.Dense(128,
+                             input_shape(4,),        #Number of nodes
+                             name ='Hidden-Layer-1', #Number of input variables
+                             activation='relu'))     #Activation function
+#Add a second hidden layer
+model.add(keras.layes.Dense(128,
+                            name='Hidden-layer-2',
+                            activation='relu'))
+
+#Add an output layer with softmax activation
