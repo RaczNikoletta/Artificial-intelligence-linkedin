bd05e68572642bf74bc70b78eda39603b2d89f27
Rácz Nikoletta <44648333+RaczNikoletta@users.noreply.github.com>
2026-02-06 07:27:34 +0100

    Enhance data preprocessing with scaling and encoding
    Added scaling and one-hot encoding for target variable.

--- deep /example1.py
+++ deep /example1.py
@@ -29,3 +29,26 @@ print(X_data[:5,:])
 print("\nTarget before scaling :\n---------------------")
 print(Y_data[:5])
 
+#Create a scaler model that is fit on the input data.
+scaler = StandardScaler().fit(X_data)
+
+#Scale the numeric feature variables
+X_data = scaler.transform(X_data)
+
+#Convert target variable as a one-hot-encoding array
+Y_data = rf.keras.utils.to_categorical(Y_data,3)
+
+print("\nFeatures after scaling :\n------------------------")
+print(X_data[:5,:])
+print("\nTarget after one-hot-encoding :\n----------------------")
+print(Y_data[:5,:])
+
+#Split training and test data
+X_train,X_test,Y_train,Y_test = train_test_split( X_data, Y_data, test_size=0.10)
+
+print ("\nTrain Test Diemnsions:\n---------------------------")
+print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+
+
+
+
